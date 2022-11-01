6ee16d792ed057d323e75629f4cff8af79666c07
Oscar Chaves <c.oscar.chaves@self.inc>
2022-11-01 11:03:16 -0600

    RT-15701: Adding embeds and representation to get transaction matches request.

--- src/Transaction/Plaid/GetPaymentMatches.ts
+++ src/Transaction/Plaid/GetPaymentMatches.ts
@@ -1,20 +1,19 @@
-import type { APICall } from "../../types";
+import type { APICall, APIFetchSettings } from "../../types";
 import getBaseURL from "../../utils/getBaseURL";
 import toQueryString, { QueryObject } from "../../utils/toQueryString";
 import getFetchConfiguration from "../../utils/getFetchConfiguration";
-import { PaymentMatch, PaymentMatchQuery, PaymentMatchAPIFetchSettings } from "./types";
+import { PaymentMatch, PaymentMatchQuery } from "./types";
 
 export default GetPaymentMatches as APICall<PaymentMatchQuery, Array<PaymentMatch>>;
 
-function GetPaymentMatches(settings: PaymentMatchAPIFetchSettings, query: PaymentMatchQuery) {
+function GetPaymentMatches(settings: APIFetchSettings, query: PaymentMatchQuery) {
   const url_path = "/api/transaction/plaid/matches";
   const base_url = getBaseURL(settings);
-  const search = toQueryString(query as unknown as QueryObject);
-  const default_body = {
-    "embeds[]": ["order", "payment_account", "verification"],
-    representation: "detailed",
-  };
-  const fetch_configuration = getFetchConfiguration({ method: "GET", body: JSON.stringify(default_body), ...settings });
+  const search = toQueryString({
+    "embeds[]": ["order", "account", "verification"],
+    ...query,
+  } as unknown as QueryObject);
+  const fetch_configuration = getFetchConfiguration({ method: "GET", ...settings });
   const final_path = `${base_url}${url_path}${search}`;
 
   return window.fetch(final_path, fetch_configuration);
