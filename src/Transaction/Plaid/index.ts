e19f95143eb8e052d08e7b8830961c4450a9bf9c
Alex Alvarez <alex.alvarez@renttrack.com>
2022-11-14 01:11:33 -0500

    RB-56: Transaction matches

--- src/Transaction/Plaid/index.ts
+++ src/Transaction/Plaid/index.ts
@@ -1,11 +1,15 @@
 import GetPaymentMatches from "./GetPaymentMatches";
 import GetLeasePaymentMatches from "./GetLeasePaymentMatches";
 import GetUtilityPaymentMatches from "./GetUtilityPaymentMatches";
+import GetTransactions from "./GetTransactions";
+import CreateTransactionMatch from "./CreateTransactionMatch";
 
 const PaymentMatchActions = {
   GetPaymentMatches,
   GetLeasePaymentMatches,
   GetUtilityPaymentMatches,
+  GetTransactions,
+  CreateTransactionMatch,
 };
 
 export default PaymentMatchActions;
