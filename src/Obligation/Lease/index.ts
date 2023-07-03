a747d74fe5e5478f1a6f06d61fd8f46380845818
Eugene Sodin <esodin@ukr.net>
2023-07-03 15:30:34 +0300

    RB-1533: Remove legacy API calls for leases

--- src/Obligation/Lease/index.ts
+++ src/Obligation/Lease/index.ts
@@ -1,7 +1,8 @@
 import CreateLease from "./CreateLease";
 import GetLeases from "./GetLeases";
+import PatchLease from "./PatchLease";
 import { LeaseActions } from "./types";
 
-const LeaseActions: LeaseActions = { GetLeases, CreateLease };
+const LeaseActions: LeaseActions = { GetLeases, CreateLease, PatchLease };
 
 export default LeaseActions;
